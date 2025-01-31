d7f1333dffdf6a3fd850353411256845c1d641ff
kais ben mustapha <kais.ben-mustapha@polymtl.ca>
2025-01-31 12:05:14 -0500

    Lecture Fichier Groupe 09

--- officiel/data/MRC_GROUPE_9/lecture fichiergroupe9.py
+++
@@ -1,6 +0,0 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-
-shapefile_path="/Users/kaisbenmustapha/Desktop/session/h25/pi4/reseau/officiel/data/MRC GROUPE 9/base_mrc_database.shp"
-gdf = gpd.read_file(shapefile_path)
- 
